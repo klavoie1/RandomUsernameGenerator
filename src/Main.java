f22ddad14d2ffc049a8b4156b8b811339224c4ba
klavoie <kenlavoie19@gmail.com>
2026-01-06 21:44:01 -0500

    Added calls to generate a random Username

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Parser.scannerParser();
+
+        String randomWordOne = RandomSelector.randomWord();
+
+        String randomWordTwo = RandomSelector.randomWord();
+
+        int randomNumber = RandomNumber.getRandomNumber();
+
+        if (Objects.equals(randomWordOne, randomWordTwo)) {
+            String updatedRandomWordTwo = RandomSelector.randomWord();
+            System.out.println("Your New Username is: " + randomWordOne + updatedRandomWordTwo + randomNumber);
+        } else {
+            System.out.println("Your New Username is: " + randomWordOne + randomWordTwo + randomNumber);
+        }
+
+
+
     }
 }
