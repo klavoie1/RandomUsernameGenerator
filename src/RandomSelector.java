c2055a1b4c2f02390c4258ba53997c0baa1de331
klavoie <kenlavoie19@gmail.com>
2026-01-06 21:45:03 -0500

    method now chooses a random word within word.txt

--- src/RandomSelector.java
+++ src/RandomSelector.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class RandomSelector {
+
+    public static String randomWord() throws IOException {
+        ArrayList<String> words = Parser.scannerParser();
+
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(words.size());
+
+        return words.get(randomNumber);
+    }
+
 }
