6cda0f71fcaae2f98abdcdc0db15703084fc1f84
klavoie <kenlavoie19@gmail.com>
2026-01-06 21:44:22 -0500

    Fixed methods, returns the array

--- src/Parser.java
+++ src/Parser.java
@@ -6,34 +6,25 @@ import java.util.Scanner;
 
 public class Parser{
 
-    static void scannerParser() throws IOException {
+    static String filename = "src/resources/word.txt";
 
-        String filename = "src/resources/word.txt";
+    static Path path = Paths.get(filename);
 
-        Path path = Paths.get(filename);
+    public static ArrayList<String> scannerParser() throws IOException {
+        
+        ArrayList<String> wordArray = new ArrayList<>();
 
         Scanner scnr = new Scanner(path);
-
-        System.out.println("Reading the Word.txt File...");
-
-        ArrayList<String> wordArray = new ArrayList<>();
 
         while (scnr.hasNextLine()) {
 
             String line = scnr.nextLine();
 
             wordArray.add(line);
-
         }
 
-        // Here for testing purposes (DELETE LATER)
-        System.out.println(wordArray);
-
         scnr.close();
+
+        return wordArray;
     }
-
-
-
-
-
 }
