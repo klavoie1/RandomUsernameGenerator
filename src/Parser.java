d7c0221b1b1281e8e5d1e74aab751d15fc3f8d76
klavoie <kenlavoie19@gmail.com>
2026-01-06 19:49:45 -0500

    Created the initial parsing code, adds to an array

--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,38 @@
-public class Parser {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parser{
+
+    static void scannerParser() throws IOException {
+
+        String filename = "src/resources/word.txt";
+
+        Path path = Paths.get(filename);
+
+        Scanner scnr = new Scanner(path);
+
+        System.out.println("Reading the Word.txt File...");
+
+        ArrayList<String> wordArray = new ArrayList<>();
+
+        while (scnr.hasNextLine()) {
+
+            String line = scnr.nextLine();
+
+            wordArray.add(line);
+
+        }
+
+        System.out.println(wordArray);
+
+        scnr.close();
+    }
+
+
+
+
+
 }
