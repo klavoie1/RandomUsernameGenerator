cbe6631d4f6a518b4cc1da4d2a5c827f8c35d709
klavoie <kenlavoie19@gmail.com>
2026-06-07 15:49:00 -0400

    Made the program run at a much faster and efficient rate.

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,48 @@
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of requests: ");
+        int requests = input.nextInt();
+        input.close();
 
-    int requests;
-    int count = 0;
-    Scanner input = new Scanner(System.in);
+        // Added list here to prevent a repetitive fetching of the word list. This was causing almost
+        // 7x the runtime. Fetching 1 million usernames went from ~7.2 sec to ~1.7 seconds
+        List<String> words = Parser.getWords();
+        int wordCount = words.size();
 
-    System.out.print("Enter number of requests: ");
-    requests = input.nextInt();
-    input.close();
+        // We are using StringBuilder here to batch output instead of printing every line immediately. The print
+        // function was unnecessarily printing each run.
+        StringBuilder output = new StringBuilder();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
 
-    while (count < requests) {
-        try {
-            String word1 = RandomSelector.randomWord();
+        for (int count = 0; count < requests; count++) {
+            String word1 = words.get(random.nextInt(wordCount));
             String word2;
+
+            // This is used to prevent the Username from being a duplicate like ForestForest3242
             do {
-                word2 = RandomSelector.randomWord();
+                word2 = words.get(random.nextInt(wordCount));
             } while (word1.equals(word2));
 
-            int suffix = RandomNumber.getRandomNumber();
+            int suffix = random.nextInt(10001);
 
-            System.out.printf("%s: %s%s%s\n", count, word1, word2, suffix);
-            count++;
+            // Fast concatenation of the username. Gets added to the output list and is displayed in "chunks"
+            output.append(count).append(": ")
+                    .append(word1).append(word2).append(suffix)
+                    .append("\n");
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            // Periodically flush the buffer to keep memory usage low. Really the program will never print more than
+            // 10 usernames at most. If there is an edge case, this will help remove RAM usage.
+            if (count % 1000 == 0) {
+                System.out.print(output);
+                output.setLength(0);
+            }
         }
-    }
-
+        System.out.print(output);
     }
 }
-
