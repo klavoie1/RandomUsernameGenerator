9eb5d1b109c290981fe2276ba734148bddb32a8d
klavoie <kenlavoie19@gmail.com>
2026-06-07 15:11:39 -0400

    Simplified Main

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        int requests = 0;
-        int count = 0;
-        Scanner input = new Scanner(System.in);
+    int requests;
+    int count = 0;
+    Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter number of requests: \n");
-        requests = input.nextInt();
-
-
-        while (count < requests) {
-            try {
-                String word1 = RandomSelector.randomWord();
-                String word2;
-                do {
-                    word2 = RandomSelector.randomWord();
-                } while (word1.equals(word2));
-
-                int suffix = RandomNumber.getRandomNumber();
-
-                System.out.printf("%s: %s%s%s\n", count, word1, word2, suffix);
-                count++;
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    System.out.print("Enter number of requests: ");
+    requests = input.nextInt();
     input.close();
+
+    while (count < requests) {
+        try {
+            String word1 = RandomSelector.randomWord();
+            String word2;
+            do {
+                word2 = RandomSelector.randomWord();
+            } while (word1.equals(word2));
+
+            int suffix = RandomNumber.getRandomNumber();
+
+            System.out.printf("%s: %s%s%s\n", count, word1, word2, suffix);
+            count++;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     }
 }
 
