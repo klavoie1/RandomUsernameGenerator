5591e0f9c8482c8f5b6a09f6586e1373c19302ed
klavoie <kenlavoie19@gmail.com>
2026-06-07 15:10:54 -0400

    Made 10000 inclusive

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -5,6 +5,6 @@ public class RandomNumber {
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     public static int getRandomNumber() {
-        return SECURE_RANDOM.nextInt(10000);
+        return SECURE_RANDOM.nextInt(10001);
     }
 }
