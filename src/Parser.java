9addbbab10da67c066e5b3221036f2e438f1632b
klavoie <kenlavoie19@gmail.com>
2026-06-10 11:25:49 -0400

    Added Javadoc to getWords function

--- src/Parser.java
+++ src/Parser.java
@@ -7,6 +7,16 @@ import java.nio.charset.StandardCharsets;
 public class Parser {
     private static List<String> cachedWords = null;
 
+    /**
+     * Retrieves a list of words from the internal 'word.txt' resource file.
+     *
+     * <p>This method reads the file once and caches the result in memory for subsequent calls.
+     * Each line in the file is trimmed, and empty lines are automatically excluded from the returned list.</p>
+     *
+     * @return An unmodifiable {@link List} of strings containing the words found in the resource file.
+     * @throws IOException If the resource 'resources/word.txt' cannot be found or an error occurs during reading.
+     * @author klavoie
+     */
     public static synchronized List<String> getWords() throws IOException {
         if (cachedWords == null) {
             try (var is = Parser.class.getResourceAsStream("resources/word.txt")) {
